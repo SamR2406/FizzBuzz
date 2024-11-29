d451d195d81844af514da933241f958173ab503b
SamuelR. <113935574+SamR2406@users.noreply.github.com>
2024-11-29 18:22:55 +0000

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz 
+
+    /* Create a for loop from 1 to 100
+     * 3 and 5 = Buzz
+     * 3 = Fizz
+     * 5 = Buzz
+     * else = number
+    */
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool threeDiv = false;
+            bool fiveDiv= false;
+
+            for (int i = 1; i<=100; i++)
+            {
+                threeDiv = i % 3 ==0;
+                fiveDiv = i % 5 ==0;
+
+                if (threeDiv && fiveDiv)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (threeDiv)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if (fiveDiv)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                    
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+                
+
+
+            }
+
+
+
+        }
+    }
+}
